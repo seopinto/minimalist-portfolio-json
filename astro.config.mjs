dbcb0fb1af5d74f1df16d75ad8308a52689b67a8
seopinto <s.andrespinto@gmail.com>
2024-04-21 21:53:36 -0500

    update github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    site: 'https://seopinto.github.io',
+    base: 'minimalist-portfolio-json',
+});
