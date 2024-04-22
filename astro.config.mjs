c0d9f450740c9a3386696fe8ee7c19f732a598ce
seopinto <s.andrespinto@gmail.com>
2024-04-22 18:31:21 -0500

    cambios para el despligue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-    site: 'https://seopinto.github.io',
-    base: 'minimalist-portfolio-json',
+    site: 'https://seopinto.github.io/minimalist-portfolio-json/',
+    base: '/minimalist-portfolio-json/',
+    output: 'static',
+    outDir: './docs',
+
+    build: {
+        assets: 'astro'
+      }
 });
